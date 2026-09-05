700d6f4df9dabec6dc9412a1c5d5620fd2716613
Momo1530 <mohammad1530@outlook.com>
2026-09-05 09:25:26 +0200

    Startseite: Kübra-Erik-Profiltext aktualisiert

--- src/app/page.js
+++ src/app/page.js
@@ -412,7 +412,7 @@ export default function Home() {
             </a>
             <a href="/team" className="ansprechperson-text-col fly-from-right delay-4" aria-label="Zum Team">
               <h3>Kübra Erik, BA</h3>
-              <p className="ansprechperson-role">Sozialarbeiter, Antigewalt- und konfrontativer Ressourcentrainer</p>
+              <p className="ansprechperson-role">Bildungswissenschaftlerin, Sozialpädagogin, Antigewalt- und konfrontative Ressourcentrainerin</p>
             </a>
           </div>
         </section>
